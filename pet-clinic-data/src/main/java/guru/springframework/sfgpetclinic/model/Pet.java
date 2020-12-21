05b3b411b1dee19bb922ccace87ab57a88088841
Manjunatha Nagaraja <nmanjunatha@gmail.com>
2020-12-20 20:32:44 -0500

    added id for model, closes #10

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -2,8 +2,12 @@ package guru.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private PetType petType;
 	private Owner owner;
 	private LocalDate birthDate;
