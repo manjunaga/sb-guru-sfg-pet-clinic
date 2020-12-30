4d467524d13923ccda8a12908f25a7c50305b3eb
Manjunatha Nagaraja <nmanjunatha@gmail.com>
2020-12-29 19:59:28 -0500

    Added PetTypes to bootstrap, Closes #30, Closes #32

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -4,8 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
 import guru.springframework.sfgpetclinic.services.VetService;
 
 @Component
@@ -13,15 +15,26 @@ public class DataLoader implements CommandLineRunner {
 
 	private final OwnerService ownerService;
 	private final VetService vetService;
+	private final PetTypeService petTypeService;
 
-	public DataLoader(OwnerService ownerService, VetService vetService) {
+	public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
 		super();
 		this.ownerService = ownerService;
 		this.vetService = vetService;
+		this.petTypeService = petTypeService;
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		PetType dog = new PetType();
+		dog.setName("Dog");
+		PetType savedDogType = petTypeService.save(dog);
+		
+		PetType cat = new PetType();
+		cat.setName("Cat");
+		PetType savedCatType = petTypeService.save(cat);
+		
 		Owner owner1 = new Owner();
 		owner1.setFirstName("Michael");
 		owner1.setLastName("Weston");
@@ -32,7 +45,7 @@ public class DataLoader implements CommandLineRunner {
 		owner2.setLastName("Glenanne");
 		ownerService.save(owner2);
 
-		System.out.println("Loaded owners...");
+		System.out.println("Loaded owners..." + ownerService.findAll().toString() );
 
 		Vet vet1 = new Vet();
 		vet1.setFirstName("Sam");
@@ -44,7 +57,7 @@ public class DataLoader implements CommandLineRunner {
 		vet2.setLastName("Triol");
 		vetService.save(vet2);
 
-		System.out.println("Loaded Vets...");
+		System.out.println("Loaded Vets..." + vetService.findAll().toString() );
 	}
 
 }
