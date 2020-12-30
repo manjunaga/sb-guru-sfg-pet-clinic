ef800587831dbcfd0bb161889cee91bc39f4f982
Manjunatha Nagaraja <nmanjunatha@gmail.com>
2020-12-29 19:41:50 -0500

    updated contact info properties to Owner, Closes #28

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -4,11 +4,11 @@ import java.util.Set;
 
 public class Owner extends Person {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	private Set<Pet> pets;
+	private String address;
+	private String city;
+	private String telephone;
 
 	public Set<Pet> getPets() {
 		return pets;
@@ -16,5 +16,29 @@ public class Owner extends Person {
 
 	public void setPets(Set<Pet> pets) {
 		this.pets = pets;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
 	}
 }
