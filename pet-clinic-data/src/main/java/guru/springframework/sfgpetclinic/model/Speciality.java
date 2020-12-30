99208b192cdb8647b41af04a508404aeb4db595f
Manjunatha Nagaraja <nmanjunatha@gmail.com>
2020-12-29 19:38:43 -0500

    added speciality class for Vet, Closes #27

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,18 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Speciality extends BaseEntity {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1900857801577707939L;
+	private String description;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
