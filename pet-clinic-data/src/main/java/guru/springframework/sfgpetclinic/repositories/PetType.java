9fc9e4c03763c7cbd489e1eef740164c220d3bc4
Manjunatha Nagaraja <nmanjunatha@gmail.com>
2020-12-31 20:00:45 -0500

    Added spring data JPA repositories, Closes #39

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/PetType.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PetType extends CrudRepository<PetType, Long> {
+
+}
